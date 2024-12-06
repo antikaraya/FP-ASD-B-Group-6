0d92269d0916f716faaa4d4635f1d5c7bf2b1a46
enqieff <5026231106@student.its.ac.id>
2024-12-06 10:49:05 +0700

    init commit

--- src/Sudoku/Puzzle.java
+++ src/Sudoku/Puzzle.java
@@ -1,60 +1,187 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1 (Jangan lupa diganti kelompok berapa)
+ * 1 - 5026231033 - Ayu Alfia Putri
+ * 2 - 5026231034 - Antika Raya
+ * 3 - 5026231106 - Nailah Qonitah Firdausa
+ */
+
 package Sudoku;
 
-public class Puzzle {
+import java.util.Random;
+import java.util.Stack;
+
+public class Puzzle{
     // All variables have package access
     // The numbers on the puzzle
-    int[][] numbers = new int[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
+    public static final int GRID_SIZE = 9;
+    public static final int SUBGRID_SIZE = 3;
+    int[][] numbers = new int[GRID_SIZE][GRID_SIZE];
+    Cell[][] puzzleCell = new Cell[GRID_SIZE][GRID_SIZE];
     // The clues - isGiven (no need to guess) or need to guess
-    boolean[][] isGiven = new boolean[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
-
+    boolean[][] isGiven = new boolean[GRID_SIZE][GRID_SIZE];
     // Constructor
-    public Puzzle() {
+    public Puzzle(){
         super();
     }
 
     // Generate a new puzzle given the number of cells to be guessed, which can be used
     //  to control the difficulty level.
     // This method shall set (or update) the arrays numbers and isGiven
-    public void newPuzzle(int cellsToGuess) {
-        // I hardcode a puzzle here for illustration and testing.
-        int[][] hardcodedNumbers =
-                {{5, 3, 4, 6, 7, 8, 9, 1, 2},
-                        {6, 7, 2, 1, 9, 5, 3, 4, 8},
-                        {1, 9, 8, 3, 4, 2, 5, 6, 7},
-                        {8, 5, 9, 7, 6, 1, 4, 2, 3},
-                        {4, 2, 6, 8, 5, 3, 7, 9, 1},
-                        {7, 1, 3, 9, 2, 4, 8, 5, 6},
-                        {9, 6, 1, 5, 3, 7, 2, 8, 4},
-                        {2, 8, 7, 4, 1, 9, 6, 3, 5},
-                        {3, 4, 5, 2, 8, 6, 1, 7, 9}};
 
-        // Copy from hardcodedNumbers into the array "numbers"
-        for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
-            for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
-                numbers[row][col] = hardcodedNumbers[row][col];
+    public void newPuzzle(int toGivenCells){
+        numbers = new int[GRID_SIZE][GRID_SIZE];
+        //sudoku generator initial value
+        int initvalue = 10;
+        int initcount = 0;
+        do {
+            for (int i=0;i<GRID_SIZE;i++){
+                for (int j=0;j<GRID_SIZE;j++){
+                    numbers[i][j]=0;
+                    puzzleCell[i][j]= new Cell(i,j,numbers[i][j]);
+                    isGiven[i][j] = false;
+                }
+            }
+            while (initcount < initvalue){
+                int rowrandoming = getRandomNumber(0, 9);
+                int colrandoming = getRandomNumber(0, 9);
+                int valrandoming = getRandomNumber(1, 9);
+                if (numbers[rowrandoming][colrandoming] == 0){
+                    if (validValue(rowrandoming, colrandoming, valrandoming)){
+                        numbers[rowrandoming][colrandoming] = valrandoming;
+                        initcount += 1;
+                        Cell temp = new Cell(rowrandoming, colrandoming,numbers[rowrandoming][colrandoming]);
+                        temp.islocked = true;
+                        puzzleCell[rowrandoming][colrandoming] = temp;
+                    }
+                }
             }
         }
+        while (!solve());
+        int appearedNumber = toGivenCells;
+        int limittedrowcol=9;
+        int count = 0;
+        int [] limitedColumn = new int[9];
+        int [] limitedRow = new int[9];
 
-        // Need to use input parameter cellsToGuess!
-        // Hardcoded for testing, only 2 cells of "8" is NOT GIVEN
-        boolean[][] hardcodedIsGiven =
-                {{true, true, true, true, true, false, true, true, true},
-                        {true, true, true, true, true, true, true, true, false},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true},
-                        {true, true, true, true, true, true, true, true, true}};
-
-        // Copy from hardcodedIsGiven into array "isGiven"
-        for (int row = 0; row < SudokuConstants.GRID_SIZE; ++row) {
-            for (int col = 0; col < SudokuConstants.GRID_SIZE; ++col) {
-                isGiven[row][col] = hardcodedIsGiven[row][col];
+        while(count<appearedNumber){
+            System.out.println(count);
+            int rowrandoming = getRandomNumber(0,9);
+            int colrandoming = getRandomNumber(0,9);
+            if(isGiven[rowrandoming][colrandoming]==false){
+                if(limitedColumn[colrandoming]<limittedrowcol&&limitedRow[rowrandoming]<limittedrowcol){
+                    isGiven[rowrandoming][colrandoming]= true;
+                    limitedColumn[colrandoming]+=1;
+                    limitedRow[rowrandoming]+=1;
+                    count+=1;
+                }
             }
         }
     }
 
-    //(For advanced students) use singleton design pattern for this class
+    public int getRandomNumber(int min, int max){
+        Random newNum = new Random();
+        int newNumber = newNum.nextInt(max)+min;
+        return newNumber;
+    }
+
+    public boolean validValue(int row, int col, int value){
+        boolean isValid = true;
+        for (int i=0;i<GRID_SIZE;i++){
+            if (numbers[i][col]==value){
+                isValid=false;
+                break;
+            }
+        }
+        for (int i=0;i<GRID_SIZE;i++){
+            if (numbers[row][i]==value){
+                isValid = false;
+                break;
+            }
+        }
+
+        int sgridrow = row-row%3;
+        int sgridcol = col-col%3;
+        for (int i=0;i<SUBGRID_SIZE;i++){
+            for (int j=0;j<SUBGRID_SIZE;j++){
+                if (numbers[sgridrow+i][sgridcol+j]==value){
+                    isValid=false;
+                    break;
+                }
+            }
+        }
+        return isValid;
+    }
+
+    public boolean solve(){
+        Stack<Cell> stack = new Stack<>();
+        int currentRow = 0;
+        int currentColumn = 0;
+        int currentValue =1;
+        while(stack.size() < 81){
+            if(puzzleCell[currentRow][currentColumn].islocked){
+                Cell lockCell = puzzleCell[currentRow][currentColumn];
+                stack.push(lockCell);
+                currentColumn+=1;
+                if (currentColumn>=9){
+                    currentRow+=1;
+                    currentColumn=0;
+                    currentValue=1;
+                }
+                continue;
+            }
+            for (;currentValue <= 10 ; currentValue++){
+                if (validValue(currentRow, currentColumn, currentValue)){
+                    break;
+                }
+            }
+
+            if(currentValue <= 9){
+                Cell cell = new Cell(currentRow, currentColumn, currentValue);
+                numbers[currentRow][currentColumn] = currentValue;
+                stack.push(cell);
+                currentColumn+=1;
+                if (currentColumn>=9){
+                    currentRow+=1;
+                    currentColumn=0;
+                }
+                currentValue = 1;
+            } else{
+                if (stack.size() > 0){
+                    // Assign to a Cell variable the top of the stack (stack.pop())
+                    Cell cell = stack.pop();
+                    // while the Cell is locked
+                    while (cell.islocked){
+                        // if stack size is greater than 0
+                        if (stack.size() > 0){
+                            // assign to the Cell variable the top of the stack (i.e. pop)
+                            cell = stack.pop();
+                        } else{
+                            // print out the number of steps (time)
+                            // return false (no solution found)
+                            System.out.println("Total steps: ");
+                            return false;
+                        }
+                    }
+
+                    // assign to currentRow the row value of the Cell
+                    currentRow = cell.row;
+                    // assign to currentColumn the col value of the Cell
+                    currentColumn = cell.col;
+                    // assign to currentValue the value of the Cell + 1
+                    currentValue = cell.number + 1;
+                    // set the value of the board Cell at currentRow, currentColumn to 0
+                    numbers[currentRow][currentColumn] =  0;
+                } else{
+                    // print out the number of steps (time)
+                    // return false (no solution found)
+                    System.out.println("Total steps: ");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- src/Sudoku/SudokuMain.java
+++ src/Sudoku/SudokuMain.java
@@ -12,6 +12,9 @@ package Sudoku;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 /**
  * The main Sudoku program
  */
@@ -19,11 +22,15 @@ public class SudokuMain extends JFrame {
     private static final long serialVersionUID = 1L;  // to prevent serial warning
 
     // private variables
-    GameBoardPanel board = new GameBoardPanel();
+    GameBoardPanel board = new GameBoardPanel(this);
     JButton btnNewGame = new JButton("New Game");
     JLabel scoreLabel = new JLabel();
+    JLabel timerLabel = new JLabel("Timer: 0 seconds");
+
 
     //declare variable
+    private Timer timer;
+    private int seconds;
     private int totalScore;
 
     // Constructor
@@ -36,22 +43,54 @@ public class SudokuMain extends JFrame {
         // Add a button to the south to re-start the game via board.newGame()
         // Add button score
         JPanel buttonPanel = new JPanel();
+        buttonPanel.add(timerLabel);
+        buttonPanel.add(scoreLabel);
+        cp.add(buttonPanel, BorderLayout.SOUTH);
 
         //set initial text for scoreLabel
         totalScore = 0; // Initialize score
         scoreLabel.setText("Your Score Now: " + totalScore);
-        buttonPanel.add(scoreLabel);
-        cp.add(buttonPanel, BorderLayout.SOUTH);
 
-        // Initialize the game board to start the game
+        // Initialize the game board and timer
+        initializeTimer();
         board.newGame();
+        totalScore = 0;
+        startTimer();
 
         pack();     // Pack the UI components, instead of using setSize()
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // to handle window-closing
         setTitle("Sudoku");
         setVisible(true);
     }
-    private void updateScoreLabel(){
+    private void initializeTimer(){
+        seconds = 0;
+        timer = new Timer(1000, new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                seconds++;
+                updateTimerLabel();
+            }
+        });
+    }
+    // Method to start the timer
+    private void startTimer(){
+        timer.start();
+    }
+
+    // Method to restart the timer
+    private void restartTimer(){
+        timer.stop();
+        seconds = 0;
+        updateTimerLabel(); // Mengupdate label timer
+        timer.start();
+    }
+
+    // Method to update the timer label
+    private void updateTimerLabel(){
+        timerLabel.setText("Timer: " + seconds + " seconds");
+    }
+
+    public void updateScoreLabel(){
+        totalScore = board.getTotalScore();
         scoreLabel.setText("Your Score Now: " + totalScore);
     }
 
